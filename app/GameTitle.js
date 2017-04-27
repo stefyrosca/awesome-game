6e2af0837404ed51c2eb8c556e3c5df2ca7b26dc
stefy <stefy_rosca2007@yahoo.com>
2017-04-27 23:50:27 +0300

    Update title & background

--- app/GameTitle.js
+++ app/GameTitle.js
@@ -5,17 +5,30 @@ class GameTitle {
 
     create() {
         console.log(this.TAG, 'create');
-        var land = this.game.add.tileSprite(0, 0, 800, 600, 'background');
-        this.gameTitle = this.game.add.sprite(200, 160, "gametitle");
-        this.gameTitle.anchor.setTo(0.5, 0.5);
-        this.playButton = this.game.add.button(200, 320, "play", this.playTheGame, this);
-        this.playButton.anchor.setTo(0.5, 0.5);
+        var land = this.game.add.tileSprite(0, 0, 1000, 1000, 'star-background');
+        this.game.stage.backgroundColor = 0x0099cc;
+
+        this.title = this.game.add.bitmapText(this.game.world.centerX - 200, this.game.world.centerY,  'carrier_command', `STAR.IO`, 50);
+        this.play = this.game.add.bitmapText(this.game.world.centerX, this.game.world.centerY + 100, 'carrier_command', `PLAY`, 30);
+        this.play.anchor.set(0.5);
+
+        this.play.inputEnabled = true;
+        this.play.events.onInputDown.add(this.playTheGame, this);
+        this.play.events.onInputOver.add(function(){
+            this.game.canvas.style.cursor = "pointer";
+        }, this);
+        this.play.events.onInputOut.add(function(){
+            this.game.canvas.style.cursor = "default";
+        }, this);
+        // this.gameTitle = this.game.add.sprite(this.game.height / 2, this.game.width / 2 - 200, "gametitle");
+        // this.gameTitle.anchor.setTo(0.5, 0.5);
+        // this.playButton = this.game.add.button(this.game.height / 2, this.game.width / 2, "play", this.playTheGame, this);
+        this.play.anchor.setTo(0.5, 0.5);
     }
 
     playTheGame() {
-        console.log('clicked !!');
-        this.gameTitle.destroy();
-        this.playButton.destroy();
+        this.title.destroy();
+        this.play.destroy();
         this.game.state.start("Game");
     }
 }
--- app/Preload.js
+++ app/Preload.js
@@ -9,9 +9,11 @@ class Preload {
         var loading = this.add.sprite(200, 240, 'loading');
         this.load.setPreloadSprite(loading);
         this.game.load.image('background', 'app/assets/background.png');
+        this.game.load.image('star-background', 'app/assets/star-background.jpg');
         this.game.load.image('player', 'app/assets/star.png');
         this.game.load.image('gametitle', 'app/assets/gametitle.png');
         this.game.load.image('play', 'app/assets/play.png');
+        this.game.load.bitmapFont('carrier_command', 'app/assets/fonts/carrier_command.png', 'app/assets/fonts/carrier_command.xml');
         //TODO: load ALL photos and logic
         // this.game.world.setBounds(-500, -500, 1000, 1000); //game world 2000 x 2000
         // this.game.physics.startSystem(Phaser.Physics.P2JS);
